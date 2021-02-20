e4397899b6bc63ad84cc26807fa85c8f439f4433
Trouvaille0198 <1375911011@qq.com>
2021-02-20 09:49:05 +0800

    11

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,22 @@
-var roleHarvester = require('role.harvester');
-
 module.exports.loop = function () {
-
-    for (var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        roleHarvester.run(creep);
+    var creep = Game.creeps['harvester1'];
+    if (creep.memory.working === true && creep.store.energy === 0) {
+        creep.memory.working = false;
+    }
+    else if (creep.memory.working === false && creep.store.energy !== 0) {
+        //start working and bring energy to the spawn
+        creep.memory.working = true;
+        
+    }
+    if (creep.memory.working === true) {
+        if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(Game.spawns['Spawn1']);
+        }
+    }
+    else {
+        var source = creep.pos.findClosestByPath(FIND_SOURCES);
+        if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(source);
+        }    
     }
 }
