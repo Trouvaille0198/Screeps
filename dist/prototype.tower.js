ce4ac65138abe55ca86342f912242e8b18871dcf
Trouvaille0198 <1375911011@qq.com>
2021-02-24 21:16:49 +0800

    pacakge tower methods

---
+++ dist/prototype.tower.js
@@ -0,0 +1,25 @@
+StructureTower.prototype._attack =
+    function () {
+        // let towers attack if necessary
+        var target = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if (target != undefined) {
+            this.attack(target);
+        }
+    }
+StructureTower.prototype._heal =
+    function () {
+        var creep = this.pos.findClosestByRange(FIND_MY_CREEPS,
+            { filter: (c) => c.hits < c.hitsMax });
+        if (creep != undefined) {
+            this.heal(creep);
+        }
+    }
+
+StructureTower.prototype._repair =
+    function () {
+        var structure = this.pos.findClosestByRange(FIND_MY_STRUCTURES,
+            { filter: (s) => s.hits < s.hitsMax && s.structureType != STRUCTURE_WALL });
+        if (structure != undefined) {
+            this.repair(structure);
+        }
+    }
