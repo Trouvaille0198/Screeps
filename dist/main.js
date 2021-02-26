1e5b567c64b6f5af4368d9d7ca1d3a32767944de
Trouvaille0198 <1375911011@qq.com>
2021-02-26 14:14:43 +0800

    抽象creep

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,4 @@
+const mount = require('mount');
 require('prototype.tower');
 require('prototype.spawn');
 var roleHarvester = require('role.harvester');
@@ -11,6 +12,8 @@ var roleClaimer = require('role.claimer');
 
 
 module.exports.loop = function () {
+    //mount
+    mount();
     //console.log(Game.rooms['E37S38'].name);
     var wholeEnergy = Game.spawns['Spawn1'].room.energyAvailable;
     var energy = wholeEnergy < 1000 ? wholeEnergy : 1000;
@@ -18,11 +21,14 @@ module.exports.loop = function () {
     //energy = 200;
     //number of creeps in different jobs
     var minNumOfHarvesters = 4;
-    var minNumOfCarriers = 2;
+    var minNumOfCarriers = 3;
     var minNumOfUpgraders = 2;
     var minNumOfRepairers = 1;
     var minNumOfBuilders = 2;
-    var minNumOfLongDistanceHarvesters = 2;
+    var minNumOfLongDistanceHarvesters = 1;
+    var minNumOfColonyBuilders = 4;
+    var minNumOfColonyUpgraders = 2;
+
     var numOfHarvesters = _.sum(Game.creeps, (creep) => creep.memory.role == 'harvester');
     var numOfCarriers = _.sum(Game.creeps, (creep) => creep.memory.role == 'carrier');
     var numOfUpgraders = _.sum(Game.creeps, (creep) => creep.memory.role == 'upgrader');
@@ -30,36 +36,47 @@ module.exports.loop = function () {
     var numOfBuilders = _.sum(Game.creeps, (creep) => creep.memory.role == 'builder');
     var numOfLongDistanceHarvesters = _.sum(Game.creeps,
         (creep) => creep.memory.role == 'longDistanceHarvester');
-
+    var numOfColonyBuilders = _.sum(Game.creeps,
+        (creep) => creep.memory.role == 'builder' && creep.memory.longDistance == true);
+    var numOfColonyUpgraders = _.sum(Game.creeps,
+        (creep) => creep.memory.role == 'upgrader' && creep.memory.longDistance == true);
 
     //Clear memories
     Game.spawns['Spawn1'].clearMemory();
+    //Game.spawns['Spawn2'].clearMemory();
     // for every creep's loop
     for (let name in Game.creeps) {
         var creep = Game.creeps[name];
-        if (creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
+        if (creep.memory.longDistance == true) {
+            if (creep.memory.role == 'builder') {
+                roleBuilder.longDistanceRun(creep, 'E37S38');
+            }
+            else if (creep.memory.role == 'upgrader') {
+                roleUpgrader.longDistanceRun(creep, 'E37S38');
+            }
         }
-        else if (creep.memory.role == 'carrier') {
-            roleCarrier.run(creep);
-        }
-        else if (creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-        else if (creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
-        }
-        else if (creep.memory.role == 'repairer') {
-            roleRepairer.run(creep);
-        }
-        else if (creep.memory.role == 'longDistanceHarvester') {
-            roleLongDistanceHarvester.run(creep);
-        }
-        else if (creep.memory.role == 'longDistanceBuilder') {
-            roleLongDistanceBuilder.run(creep);
-        }
-        else if (creep.memory.role == 'claimer') {
-            roleClaimer.run(creep);
+        else {
+            if (creep.memory.role == 'harvester') {
+                roleHarvester.run(creep);
+            }
+            else if (creep.memory.role == 'carrier') {
+                roleCarrier.run(creep);
+            }
+            else if (creep.memory.role == 'upgrader') {
+                roleUpgrader.run(creep);
+            }
+            else if (creep.memory.role == 'builder') {
+                roleBuilder.run(creep);
+            }
+            else if (creep.memory.role == 'repairer') {
+                roleRepairer.run(creep);
+            }
+            else if (creep.memory.role == 'longDistanceHarvester') {
+                roleLongDistanceHarvester.run(creep);
+            }
+            // else if (creep.memory.role == 'claimer') {
+            //     roleClaimer.run(creep);
+            // }
         }
     }
 
@@ -81,6 +98,12 @@ module.exports.loop = function () {
     else if (numOfLongDistanceHarvesters < minNumOfLongDistanceHarvesters) {
         Game.spawns['Spawn1'].spawnLongDistanceHarvester('E37S37', 'E37S38');
     }
+    else if (numOfColonyUpgraders < minNumOfColonyUpgraders) {
+        Game.spawns['Spawn1'].spawnCustomCreep(400, 'upgrader', true);
+    }
+    else if (numOfColonyBuilders < minNumOfColonyBuilders) {
+        Game.spawns['Spawn1'].spawnCustomCreep(400, 'builder', true);
+    }
 
 
     //towers' actions
@@ -97,4 +120,5 @@ module.exports.loop = function () {
     console.log('Har: ' + numOfHarvesters, 'Car: ' + numOfCarriers, 'Upgr: ' + numOfUpgraders,
         'Repair: ' + numOfRepairers, 'Build: ' + numOfBuilders,
         'LongDi: ' + numOfLongDistanceHarvesters);
+    console.log('longBuilder: ' + numOfColonyBuilders, 'longUp: ' + numOfColonyUpgraders);
 } 
---
+++ dist/mountCreep.js
@@ -0,0 +1,268 @@
+// 将拓展签入 Creep 原型
+module.exports = function () {
+    _.assign(Creep.prototype, creepExtension);
+}
+
+const creepExtension = {
+    _checkWorkingState() {
+        //check working state ang switch it if necessary
+        if (this.store[RESOURCE_ENERGY] == 0) {
+            //if has no energy, stop working and go to get energy
+            this.memory.working = false;
+        }
+        else if (this.store[RESOURCE_ENERGY] == this.store.getCapacity(RESOURCE_ENERGY)) {
+            //if carried energy full, go working
+            this.memory.working = true;
+        }
+    },
+
+    _harvestEnergy() {
+        //harvest energy from the source
+        var source = this.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+        if (source != undefined) {
+            if (this.harvest(source) == ERR_NOT_IN_RANGE) {
+                this.moveTo(source);
+                return true;
+            }
+        }
+        else {
+            this.say('莫得东西采');
+            return false;
+        }
+    },
+
+    _transferEnergyToContainer() {
+        //bring energy to the container
+        var structure = this.pos.findClosestByPath(FIND_STRUCTURES,
+            {
+                filter: (s) => (s.structureType == STRUCTURE_CONTAINER
+                    && s.store[RESOURCE_ENERGY] < s.store.getCapacity(RESOURCE_ENERGY))
+            });
+        if (structure != undefined) {
+            if (this.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                this.moveTo(structure);
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+    },
+
+    _pickupEnergy() {
+        //pick up dropped energy
+        var dropEnergy = this.pos.findClosestByPath(FIND_DROPPED_RESOURCES,
+            {
+                filter: (s) => s.resourceType == RESOURCE_ENERGY
+            });
+        if (dropEnergy != undefined) {
+
+            this.say('捡垃圾咯')
+            if (this.pickup(dropEnergy) == ERR_NOT_IN_RANGE) {
+                this.moveTo(dropEnergy);
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+    },
+
+    _withdrawEnergyFromRuin() {
+        //withdraw energy from ruin
+        var ruin = this.pos.findClosestByPath(FIND_RUINS);
+        if (ruin != undefined) {
+            //pick up drop energy
+            if (this.withdraw(ruin, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                this.moveTo(ruin);
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+    },
+
+    _withdrawEnergyFromContainer() {
+        //get energy from the container 
+        var structure = this.pos.findClosestByPath(FIND_STRUCTURES,
+            {
+                filter: (s) => (s.structureType == STRUCTURE_CONTAINER
+                    && s.store[RESOURCE_ENERGY] != 0)
+            });
+        if (structure != undefined) {
+            if (this.withdraw(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                this.moveTo(structure);
+                return true;
+            }
+        }
+        else {
+            this.say('无物可搬');
+            return false;
+        }
+    },
+
+    _transferEnergyToSpawn() {
+        //carry energy to spawn
+        var structure = this.pos.findClosestByPath(FIND_MY_STRUCTURES,
+            {
+                filter: (s) => (s.structureType == STRUCTURE_SPAWN
+                    && s.store[RESOURCE_ENERGY] < s.store.getCapacity(RESOURCE_ENERGY))
+            });
+        if (structure != undefined) {
+            if (this.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                this.moveTo(structure);
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+
+    },
+
+    _transferEnergyToExtension() {
+        //carry energy to extention
+        var structure = this.pos.findClosestByPath(FIND_MY_STRUCTURES,
+            {
+                filter: (s) => (s.structureType == STRUCTURE_EXTENSION
+                    && s.store[RESOURCE_ENERGY] < s.store.getCapacity(RESOURCE_ENERGY))
+            });
+        if (structure != undefined) {
+            if (this.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                this.moveTo(structure);
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+
+    },
+
+    _transferEnergyToTower() {
+        //carry energy to tower
+        var structure = this.pos.findClosestByPath(FIND_MY_STRUCTURES,
+            {
+                filter: (s) => (s.structureType == STRUCTURE_TOWER
+                    && s.store[RESOURCE_ENERGY] < s.store.getCapacity(RESOURCE_ENERGY))
+            });
+        if (structure != undefined) {
+            if (this.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                this.moveTo(structure);
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+
+    },
+
+    _transferEnergyToStorage() {
+        //carry energy to storage
+        var structure = this.pos.findClosestByPath(FIND_MY_STRUCTURES,
+            {
+                filter: (s) => (s.structureType == STRUCTURE_STORAGE
+                    && s.store[RESOURCE_ENERGY] < s.store.getCapacity(RESOURCE_ENERGY))
+            });
+        if (structure != undefined) {
+            if (this.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                this.moveTo(structure);
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+    },
+
+    _withdrawEnergyFromStorage() {
+        var structure = this.pos.findClosestByPath(FIND_MY_STRUCTURES,
+            {
+                filter: (s) => (s.structureType == STRUCTURE_STORAGE
+                    && s.store[RESOURCE_ENERGY] > 0)
+            });
+        if (structure != undefined) {
+            if (this.withdraw(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                this.moveTo(structure);
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+    },
+
+    _withdrawEnergyFromExtension() {
+        var structure = this.pos.findClosestByPath(FIND_MY_STRUCTURES,
+            {
+                filter: (s) => (s.structureType == STRUCTURE_EXTENSION
+                    && s.store[RESOURCE_ENERGY] > 0)
+            });
+        if (structure != undefined) {
+            if (this.withdraw(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                this.moveTo(structure);
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+    },
+
+    _build() {
+        // complete a constructionSite
+        var constructionSite = this.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+        if (constructionSite != undefined) {
+            // if there are some construction sites to build
+            if (this.build(constructionSite) == ERR_NOT_IN_RANGE) {
+                this.moveTo(constructionSite);
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+    },
+
+    _repair() {
+        var structure = this.pos.findClosestByPath(FIND_STRUCTURES,
+            {
+                filter: (s) => s.hits < s.hitsMax && s.structureType != STRUCTURE_WALL
+                    && s.structureType != STRUCTURE_RAMPART
+            });
+        var wall = this.pos.findClosestByPath(FIND_STRUCTURES,
+            {
+                filter: (s) => (s.structureType == STRUCTURE_WALL || s.structureType == STRUCTURE_RAMPART)
+                    && s.hits < 7000
+            });
+        if (structure != undefined) {
+            if (this.repair(structure) == ERR_NOT_IN_RANGE) {
+                this.moveTo(structure);
+                return true;
+            }
+        }
+        else if (wall != undefined) {
+            if (this.repair(wall) == ERR_NOT_IN_RANGE) {
+                this.moveTo(wall);
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+    },
+
+    _goToAnotherRoom(targetRoom) {
+        if (this.room.name != targetRoom) {
+            this.say('爬到另一个房间去了');
+            var exit = this.room.findExitTo(targetRoom);
+            this.moveTo(this.pos.findClosestByRange(exit));
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
