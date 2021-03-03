3c809ecd620c3e43e8351153d113dd8995de6999
Tyeah <1375911011@qq.com>
2021-03-03 15:03:54 +0800

    delete some ways in getting energy of builder

--- dist/role.builder.js
+++ dist/role.builder.js
@@ -7,11 +7,7 @@ var roleBuilder = {
         if (creep.memory.working == false) {
             //get energy from the energy-stored-structure
             if (!creep._withdrawEnergyFromStorage()) {
-                if (!creep._withdrawEnergyFromExtension()) {
-                    if (!creep._withdrawEnergyFromContainer()) {
-                        creep._harvestEnergy();
-                    } 
-                }
+                creep._harvestEnergy();     
             }
         }
         else {
